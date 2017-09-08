a314a959b99240a05c4253d9714cd24a5fe61657
castarox <mika.kamil12@gmail.com>
2017-09-08 22:41:40 +0200

    Create route to add new Track

--- src/main/java/com/example/hacksilesia/track/TrackController.java
+++ src/main/java/com/example/hacksilesia/track/TrackController.java
@@ -1,10 +1,27 @@
 package com.example.hacksilesia.track;
 
+import com.example.hacksilesia.form.TrackForm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
 
 @Controller
 public class TrackController {
 
+    @Autowired
+    private TrackService trackService;
+
+    @PostMapping("/track/new")
+    @ResponseBody
+    Track addTrack(@ModelAttribute("form") @Valid TrackForm trackForm, BindingResult result){
+        if(trackService.isCorrectForm(result.)){
+            return trackService.createTrack();
+        }
+        return null;
+    }
 }
