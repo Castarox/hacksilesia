94e2e7f41ff51bf42a7e995fab30eb3241b5ad2c
castarox <mika.kamil12@gmail.com>
2017-09-08 22:53:34 +0200

    Create method createTrack in TrackService;

--- src/main/java/com/example/hacksilesia/track/TrackService.java
+++ src/main/java/com/example/hacksilesia/track/TrackService.java
@@ -1,5 +1,6 @@
 package com.example.hacksilesia.track;
 
+import com.example.hacksilesia.form.TrackForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
@@ -13,4 +14,29 @@ public class TrackService {
     public boolean isCorrectForm(BindingResult result) {
         return result.hasErrors();
     }
+
+    public Track createTrack(TrackForm trackForm) {
+        String name = trackForm.getName();
+        Double start_latitude = trackForm.getStart_latitude();
+        Double start_longitude = trackForm.getStart_longitude();
+        Double end_latitude = trackForm.getEnd_latitude();
+        Double end_longitude = trackForm.getStart_longitude();
+        Integer space = trackForm.getSpace();
+        String telephone = trackForm.getTelephone();
+        String time = trackForm.getTime();
+        String other = trackForm.getOther();
+        return new Track(
+                name,
+                start_latitude,
+                start_longitude,
+                end_latitude,
+                end_longitude,
+                space,
+                telephone,
+                time,
+                other);
+    }
+
+
+
 }
