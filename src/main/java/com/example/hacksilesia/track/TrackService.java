c3b69db81208978f19b43176fe5daf134962c729
castarox <mika.kamil12@gmail.com>
2017-09-08 22:45:37 +0200

    Create isCorrectForm method

--- src/main/java/com/example/hacksilesia/track/TrackService.java
+++ src/main/java/com/example/hacksilesia/track/TrackService.java
@@ -1,7 +1,16 @@
 package com.example.hacksilesia.track;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
 
 @Service
 public class TrackService {
+
+    @Autowired
+    private TrackRepository trackRepository;
+
+    public boolean isCorrectForm(BindingResult result) {
+        return result.hasErrors();
+    }
 }
