b33dcb6387989d1ac60472f8fb82e2eef6188a8f
castarox <mika.kamil12@gmail.com>
2017-09-08 21:59:15 +0200

    create project

---
+++ src/main/java/com/example/hacksilesia/HacksilesiaApplication.java
@@ -0,0 +1,12 @@
+package com.example.hacksilesia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacksilesiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacksilesiaApplication.class, args);
+	}
+}
