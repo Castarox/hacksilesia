90f74544f2c005d782959c8aaac52628403139d5
castarox <mika.kamil12@gmail.com>
2017-09-08 22:47:43 +0200

    Add getters and setters to TrackForm

--- src/main/java/com/example/hacksilesia/form/TrackForm.java
+++ src/main/java/com/example/hacksilesia/form/TrackForm.java
@@ -21,4 +21,76 @@ public class TrackForm {
     @NotNull
     private String time;
     private String other;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getStart_latitude() {
+        return start_latitude;
+    }
+
+    public void setStart_latitude(Double start_latitude) {
+        this.start_latitude = start_latitude;
+    }
+
+    public Double getStart_longitude() {
+        return start_longitude;
+    }
+
+    public void setStart_longitude(Double start_longitude) {
+        this.start_longitude = start_longitude;
+    }
+
+    public Double getEnd_latitude() {
+        return end_latitude;
+    }
+
+    public void setEnd_latitude(Double end_latitude) {
+        this.end_latitude = end_latitude;
+    }
+
+    public Double getEnd_longitude() {
+        return end_longitude;
+    }
+
+    public void setEnd_longitude(Double end_longitude) {
+        this.end_longitude = end_longitude;
+    }
+
+    public Integer getSpace() {
+        return space;
+    }
+
+    public void setSpace(Integer space) {
+        this.space = space;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getOther() {
+        return other;
+    }
+
+    public void setOther(String other) {
+        this.other = other;
+    }
 }
