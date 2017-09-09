a13f8cf87e23f17e0c87ce4000c81155d7d4714c
cj <sigamateusz@gmail.com>
2017-09-09 11:36:09 +0200

    Add infowindows for markers

--- src/main/resources/static/js/passenger-locations.js
+++ src/main/resources/static/js/passenger-locations.js
@@ -1,4 +1,5 @@
 var map;
+var address;
 function initMap() {
     var poland_center = {lat: 52.151445, lng: 19.158053};
     map = new google.maps.Map(document.getElementById('map'), {
@@ -20,22 +21,57 @@ function setStartLatLon() {
     var place = autocomplete_start.getPlace();
     var latitude = place.geometry.location.lat();
     var longitude = place.geometry.location.lng();
-    var distance = 5;
+    var distance = 1;
 
     $.ajax({
     url: '/passenger/start/' + latitude + '/' + longitude + '/' + distance,
     type: 'get',
     success: function (data) {
         data.forEach(function (track) {
-            var track_position = {lat: track['startLatitude'], lng: track['startLongitude']};
-            new google.maps.Marker({
-            position: track_position,
-            map: map
+            var lat = track['startLatitude'];
+            var lng = track['startLongitude'];
+            var track_position = {lat: lat, lng: lng};
+            var link = '/track/' +  track['id'];
+            var time = track['time'];
+            var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=AIzaSyClhLo7xcQp1G13D7VSR65oekjtwj1Llsc";
+            var address;
+            $.ajax({
+                url: url,
+                type: 'get',
+                success: function (googleObj) {
+                    address = googleObj.results[1].formatted_address;
+                    console.log(address);
+                    var contentString =
+                    '<div id="content">'+
+                    '<div id="siteNotice">'+
+                    '</div>'+
+                    '<h1 id="firstHeading" class="firstHeading">' + time + '</h1>'+
+                    '<div id="bodyContent">'+
+                    '<p><h3><b>JEDZIE DO :' + address + ' </b></h3>' +
+                    '<a href=' + link + '>'+
+                    'LINK DO OGŁOSZENIA</a> '+
+                    '</div>'+
+                    '</div>';
+                    var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                    });
+                    var marker = new google.maps.Marker({
+                    position: track_position,
+                    map: map,
+                    title: track['name']
+                    });
+                    marker.addListener('click', function() {
+                    infowindow.open(map, marker);
+                    });
+                    },
+                error: function (e) {
+                    alert(e);
+                }
             });
         });
         var center = new google.maps.LatLng(latitude, longitude);
         map.panTo(center);
-        smoothZoom(map, 16, map.getZoom());
+        smoothZoom(map, 17, map.getZoom());
     },
         error: function (e) {
            console.log(e);
@@ -54,4 +90,20 @@ function smoothZoom (map, max, cnt) {
         });
         setTimeout(function(){map.setZoom(cnt)}, 150);
     }
+}
+
+function getAddress(lat, lng) {
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=AIzaSyClhLo7xcQp1G13D7VSR65oekjtwj1Llsc";
+    $.ajax({
+        url: url,
+        type: 'get',
+        success: function (googleObj) {
+            console.log(googleObj.results[1].formatted_address);
+            address = googleObj.results[1].formatted_address;
+
+        },
+        error: function (e) {
+            alert(e);
+        }
+    })
 }
