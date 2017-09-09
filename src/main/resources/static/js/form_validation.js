96d1e995cfe7d166cbcfba0a2f158df8317d4748
Ania Polak <tunczykzcytryna@gmail.com>
2017-09-09 09:15:47 +0200

    Change validation function

--- src/main/resources/static/js/form_validation.js
+++ src/main/resources/static/js/form_validation.js
@@ -1,35 +1,10 @@
-var addForm = document.forms['add-form'];
-addForm.addEventListener('submit', validate);
+function validate() {
 
-function validate(evt) {
-    evt.preventDefault();
+        var f = document.getElementById("add-form").elements;
+        var isDataValid = true;
 
-    if (addForm.name.value.length < 3 || addForm.name.value.value == "" ) {
-        addForm.name.style.borderColor="#E33054";
-        return false;
-    }
-    if (addForm.start.value.length < 3 || addForm.start.value.length =="" ) {
-        addForm.start.style.borderColor="#E33054";
-        return false;
-    }
-    if (addForm.end.value.length < 3 || addForm.end.value.length =="" ) {
-        addForm.end.style.borderColor="#E33054";
-        return false;
-    }
-    if (addForm.contact.value.length < 3 || addForm.contact.value.length =="" ) {
-        addForm.contact.style.borderColor="#E33054";
-        return false;
-    }
-    if (addForm.time.value.length < 3 || addForm.time.value.length =="" ) {
-        addForm.time.style.borderColor="#E33054";
-        return false;
-    }
-    if (addForm.others.value.length < 3 || addForm.others.value.length =="" ) {
-        addForm.others.style.borderColor="#E33054";
-        return false;
-    }
-
-
-
-    addForm.submit();
+        for (var i = 0; i < f.length; i++) {
+            if (f[i].value.length == 0) isDataValid = false;
+        }
+    document.getElementById('sbt').disabled = !isDataValid;
 }
