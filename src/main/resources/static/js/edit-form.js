f50bdec3f2292d161c357c2baba7d572dd6ee2ce
castarox <mika.kamil12@gmail.com>
2017-09-09 15:18:42 +0200

    Edit edit form

--- src/main/resources/static/js/edit-form.js
+++ src/main/resources/static/js/edit-form.js
@@ -1,40 +1,5 @@
 $(document).ready(function () {
     // setTimeout(startSet, 3000);
-    $(window).load(startSet());
-    $(window).load(endSet());
-    function startSet() {
-        var start_lati = $('#start').data("latitude");
-        var start_long = $('#start').data("longitude");
-        var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ start_lati + "," + start_long + "&key=AIzaSyClhLo7xcQp1G13D7VSR65oekjtwj1Llsc";
-        $.ajax({
-            url: url,
-            type: 'get',
-
-            success: function (data) {
-                $('#start').val(data.results[1].formatted_address)
-            },
-            error: function (e) {
-                alert("dupa")
-            }
-        });
-    }
-
-    function endSet() {
-        var end_lati = $('#end').data("latitude");
-        var end_long = $('#end').data("longitude");
-        var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ end_lati + "," + end_long + "&key=AIzaSyClhLo7xcQp1G13D7VSR65oekjtwj1Llsc";
-        $.ajax({
-            url: url,
-            type: 'get',
-
-            success: function (data) {
-                $('#end').val(data.results[1].formatted_address)
-            },
-            error: function (e) {
-                alert("dupa")
-            }
-        });
-    }
 
     $("#submit").click(function()  {
         $('.form').preventDefault();
@@ -43,10 +8,8 @@ $(document).ready(function () {
         var contact = $('#contact').val();
         var time = $('#time').val();
         var others = $('#others').val();
-        var start_latitude = $('#start').data("latitude");
-        var start_longitude = $('#start').data("longitude");
-        var end_latitude = $('#end').data("latitude");
-        var end_longitude = $('#end').data("longitude");
+        var startCity = $('#start').val();
+        var endCity = $('#end').val();
         var link = $('#name').data("link");
         var url = "/track/" + link;
         alert(url);
@@ -56,10 +19,8 @@ $(document).ready(function () {
             contentType: "application/json",
             dataType: 'json',
             data: JSON.stringify({name: name,
-                start_latitude: start_latitude,
-                start_longitude: start_longitude,
-                end_latitude: end_latitude,
-                end_longitude: end_longitude,
+                startCity: startCity,
+                endCity: endCity,
                 space:select,
                 telephone:contact,
                 time:time,
