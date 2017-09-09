c4ffc141fb588b61b5d5b6b7990d77e6de5d43c1
cj <sigamateusz@gmail.com>
2017-09-09 05:51:02 +0200

    Clean code

--- src/main/resources/static/js/passenger-locations.js
+++ src/main/resources/static/js/passenger-locations.js
@@ -1,16 +1,18 @@
 function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new google.maps.Map(document.getElementById('map'), {
-            zoom: 4,
-            center: uluru
-        });
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
     var marker = new google.maps.Marker({
             position: uluru,
             map: map
         });
+    var user_input = document.getElementById('start');
     autocomplete_start = new google.maps.places.Autocomplete(
-    /** @type {!HTMLInputElement} */(document.getElementById('start')),
-    {types: ['geocode']});
+        (user_input),
+        {types: ['geocode']}
+        );
     autocomplete_start.setComponentRestrictions(
         {'country': ['pl']}
     );
@@ -21,9 +23,9 @@ function initMap() {
     }
 
 function setStartLatLon() {
-var place = autocomplete_start.getPlace();
-var latitude = place.geometry.location.lat();
-var longitude = place.geometry.location.lng();
-document.getElementById('start').setAttribute('data-latitude',latitude);
-document.getElementById('start').setAttribute('data-longitude',longitude);
+    var place = autocomplete_start.getPlace();
+    var latitude = place.geometry.location.lat();
+    var longitude = place.geometry.location.lng();
+    document.getElementById('start').setAttribute('data-latitude',latitude);
+    document.getElementById('start').setAttribute('data-longitude',longitude);
 }
