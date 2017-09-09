9c441d2b920e3015f8f2a981f006e52c7cb758bc
Anna Matras <ania.matras@gmail.com>
2017-09-09 04:04:08 +0000

    Add user entity

--- src/main/java/com/example/hacksilesia/user/User.java
+++ src/main/java/com/example/hacksilesia/user/User.java
@@ -10,6 +10,57 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
+    private String surname;
+    private String mail;
+    private String password;
 
     public User(){}
+
+    public User(String name, String surname, String mail, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/example/hacksilesia/user/UserController.java
+++ src/main/java/com/example/hacksilesia/user/UserController.java
@@ -2,17 +2,31 @@ package com.example.hacksilesia.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("user")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping("")
-    String index() {
+    String index(Model model, ModelMap modelMap) {
+        User user = (User) modelMap.get("user");
+
         return "index";
+    }
+
+    @GetMapping("/login")
+    String logInToTheSiteViewPage(ModelMap model){
+        if (model.get("user") == null) {
+            return "login";
+        }
+        return "redirect:/";
     }
 
     @GetMapping("/about")
