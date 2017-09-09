2bd43f581d6a5823c6fc60511c42af972335431e
castarox <mika.kamil12@gmail.com>
2017-09-09 06:11:22 +0200

    Complete edit form script

--- src/main/java/com/example/hacksilesia/track/TrackRest.java
+++ src/main/java/com/example/hacksilesia/track/TrackRest.java
@@ -3,7 +3,9 @@ package com.example.hacksilesia.track;
 import com.example.hacksilesia.form.TrackForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,8 @@ import javax.validation.Valid;
 
 @RestController
 public class TrackRest {
+    @Autowired
+    TrackRepository trackRepository;
 
     @Autowired
     TrackService trackService;
@@ -23,6 +27,17 @@ public class TrackRest {
         if (trackService.isCorrectForm(errors)) {
             Track track = trackService.createTrack(trackForm);
             track.setLink(trackService.generateLink(track));
+            trackService.save(track);
+            return track;
+        }
+        return null;
+    }
+
+    @PostMapping("track/{link}")
+    Track editTrack(@Valid @RequestBody TrackForm trackForm, Errors errors, @PathVariable String link){
+        if (trackService.isCorrectForm(errors)) {
+            Track track = trackRepository.findByLink(link);
+            trackService.editTrack(track, trackForm);
             return track;
         }
         return null;
