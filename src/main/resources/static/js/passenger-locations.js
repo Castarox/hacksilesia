e0b9a831d2ec2146a415cc56450dc27cdbe73f5f
cj <sigamateusz@gmail.com>
2017-09-09 05:48:42 +0200

    Add map & autocomplete for passenger page

---
+++ src/main/resources/static/js/passenger-locations.js
@@ -0,0 +1,29 @@
+function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+    var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+        });
+    autocomplete_start = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('start')),
+    {types: ['geocode']});
+    autocomplete_start.setComponentRestrictions(
+        {'country': ['pl']}
+    );
+    autocomplete_start.addListener('place_changed', setStartLatLon);
+
+
+
+    }
+
+function setStartLatLon() {
+var place = autocomplete_start.getPlace();
+var latitude = place.geometry.location.lat();
+var longitude = place.geometry.location.lng();
+document.getElementById('start').setAttribute('data-latitude',latitude);
+document.getElementById('start').setAttribute('data-longitude',longitude);
+}
