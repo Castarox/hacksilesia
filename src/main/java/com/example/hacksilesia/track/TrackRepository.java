b14c78c60a4d1e0ff2ef6c8d49ce8010a5376365
cj <sigamateusz@gmail.com>
2017-09-09 08:53:20 +0200

    Add query for finding markers on map

--- src/main/java/com/example/hacksilesia/track/TrackRepository.java
+++ src/main/java/com/example/hacksilesia/track/TrackRepository.java
@@ -3,9 +3,15 @@ package com.example.hacksilesia.track;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.*;
+
 @Repository
 public interface TrackRepository extends CrudRepository<Track, Long> {
 
     Track findByLink(String link);
     Track findById(Long id);
+    List<Track> findByStartLatitudeLessThanEqualAndStartLatitudeIsGreaterThanEqualAndStartLongitudeIsLessThanEqualAndStartLongitudeGreaterThanEqual(double maxLatitude,
+                                                                                                                                                    double minLatitude,
+                                                                                                                                                    double maxLongitude,
+                                                                                                                                                    double minLongitude);
 }
