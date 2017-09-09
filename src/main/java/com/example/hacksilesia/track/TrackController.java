177fb4db29a242a27d45bacdd5c56c2ffb3f2977
Ania Polak <tunczykzcytryna@gmail.com>
2017-09-09 12:59:33 +0200

    Delete unused imports

--- src/main/java/com/example/hacksilesia/track/TrackController.java
+++ src/main/java/com/example/hacksilesia/track/TrackController.java
@@ -1,19 +1,10 @@
 package com.example.hacksilesia.track;
 
-import com.example.hacksilesia.form.TrackForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.validation.Valid;
 
 @Controller
 public class TrackController {
